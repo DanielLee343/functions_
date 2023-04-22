c464c43c67d2d209720ee2be834dd24c10de49f2
Chameleon Cloud User <cc@uc-inst-1.novalocal>
2023-04-22 00:34:22 +0000

    0421 test1

--- run_bench/normal_run/graph_.py
+++ run_bench/normal_run/graph_.py
@@ -3,14 +3,19 @@ import sys, os
 from time import time, sleep
 import subprocess
 from datetime import datetime
+import random
+import shutil
 
 CUR_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 graph_dir = CUR_DIR + "/graph_dir"
+VTUNE = "/opt/intel/oneapi/vtune/2023.1.0/bin64/vtune"
+PROF_DIR_BASE= "/home/cc/functions/run_bench/vtune_log"
 
 def main():
     algo = sys.argv[1]
     size = sys.argv[2]
-    file_name = graph_dir + "/" + size + ".pkl"
+    run_vtune = sys.argv[3]
+    get_heatmap = sys.argv[4]
 
     # gen graph
     graph_generating_begin = time()
@@ -19,19 +24,28 @@ def main():
 
     # read graph
     start_loading = time()
-    graph = igraph.Graph.Read(f=file_name, format="pickle") # mmap() -> 30s
+    if size == "twitter" or size == "COLLAB_A" or size == "aspirin_A" or size == "twitter_rv" :
+        file_name = graph_dir + "/" + size + ".el"
+        graph = igraph.Graph.Read(f=file_name, format="edgelist")
+    else:
+        file_name = graph_dir + "/" + size + ".pkl"
+        graph = igraph.Graph.Read(f=file_name, format="pickle")
     read_time = time() - start_loading
-    print("VA of graph:", hex(id(graph)))
+    print("VA of graph:", hex(id(graph)), "decimal is: ", id(graph))
 
     # compute
     check_pid = str(os.getpid())
-    subprocess.Popen(["sudo","damo","record", "--monitoring_nr_regions_range", "1000", "2000", "-o", 
-                        "/home/cc/functions/run_bench/playground/"+algo+"/"+algo+".data", check_pid])
+    if get_heatmap == "true":
+        damo_trace = "/home/cc/functions/run_bench/playground/"+ algo + "_" + size + "/" + algo + "_" + size + ".data"
+        subprocess.Popen(["sudo","damo","record", "--monitoring_nr_regions_range", "1000", "2000", "-o", 
+                        damo_trace, check_pid])
+    if run_vtune == "true":
+        PROF_DIR = os.path.join(PROF_DIR_BASE, algo + "_" + size)
+        if os.path.isdir(PROF_DIR):
+            shutil.rmtree(PROF_DIR)
+        subprocess.Popen([VTUNE, "-collect", "uarch-exploration", "-r", 
+                          PROF_DIR, "-target-pid", check_pid])
     process_begin = time()
-
-    # now = datetime.now()
-    # current_time = now.strftime("%H:%M:%S")
-    # print("start computing =", current_time)
 
     if algo == 'pagerank':
         result = graph.pagerank()
@@ -39,8 +53,12 @@ def main():
         result = graph.spanning_tree(None, False)
     elif algo == 'bfs':
         result = graph.bfs(0)
+    elif algo == 'clusters':
+        result = graph.clusters(mode='strong')
+    elif algo == 'biconnect':
+        result = graph.biconnected_components()
     else: 
-        print("wrong algo!")
+        print("unsupported algo!")
         return "err"
     process_time = time() - process_begin
     # now = datetime.now()
@@ -57,4 +75,43 @@ def main():
         "read_time: ", "{:.2f}".format(read_time),
         "compute_time: ", "{:.2f}".format(process_time))
     
+def dump_graph():
+    size = "twitter_rv"
+    graph_generating_begin = time()
+    graph = igraph.Graph.Barabasi(int(size), 10)
+    graph_gen_time = time() - graph_generating_begin
+
+    graph_dump_begin = time()
+
+    # print(g)
+    # graph.write_edgelist(f=graph_dir+"/{}.el".format(size))
+    # graph.write_pickle(fname="/home/cc/functions/run_bench/normal_run/graph_dir/{}.pkl".format(size))
+    # graph.write_graphmlz(f="/home/cc/functions/run_bench/normal_run/graph_dir/{}_graphmlz".format(size), compresslevel=9)
+    # graph_graphmlz = igraph.Graph.Read_GraphMLz(f=graph_dir + "/{}_graphmlz".format(size))
+    graph_dump_time = time() - graph_dump_begin
+
+    graph_pickle_read_begin = time()
+    graph_pickle = igraph.Graph.Read(f=graph_dir + "/{}.el".format(size), format="edgelist")
+    # graph_pickle = igraph.Graph.Read(f=graph_dir + "/{}.pkl".format(size), format="pickle")
+    graph_pickle_load_time = time() - graph_pickle_read_begin
+
+    print("graph_gen_time: ", "{:.2f}".format(graph_gen_time),
+        "graph_dump_time: ", "{:.2f}".format(graph_dump_time),
+        "graph_picke_load_time: ", "{:.2f}".format(graph_pickle_load_time))
+    process_begin = time()
+    graph_pickle.pagerank()
+    pr_done = time()
+    print("pr:", pr_done - process_begin)
+    graph_pickle.spanning_tree(None, False)
+    mst_done = time()
+    print("mst:", mst_done - pr_done)
+    graph_pickle.bfs(0)
+    graph_pickle.mincut(None, None, None)
+    bfs_done = time()
+    print("bfs:", bfs_done - mst_done)
+    graph_pickle.biconnected_components()
+    biconnect_done = time()
+    print("biconnect:", biconnect_done - bfs_done)
+
 main()
+# dump_graph()
--- run_bench/run_vtune.sh
+++ run_bench/run_vtune.sh
@@ -87,17 +87,24 @@ run_workload()
     # which workload
     local func=$2
     # bm (bare metal), faas, add later
-    local level=$3
+    local wl_args=$3
 
     get_rss=true
     get_bw=true
     get_heatmap=true
-
-    if [ ! -d "$PLAYGROUND_DIR/$func" ]; then
-        # echo "does not exists"
-        mkdir -p $PLAYGROUND_DIR/$func
+    run_vtune=true
+    if [ ! -z $wl_args ]; then
+        wl_folder="$func"_"$wl_args"
     else
-        rm -rf $PLAYGROUND_DIR/$func/*
+        wl_folder=$func
+    fi
+    echo "folder name in playground:" $wl_folder
+
+    if [ ! -d "$PLAYGROUND_DIR/$wl_folder" ]; then
+        # echo "does not exists"
+        mkdir -p $PLAYGROUND_DIR/$wl_folder
+    else
+        rm -rf $PLAYGROUND_DIR/$wl_folder/*
     fi
     echo "running $func in $env"
     if [ "$env" = "cxl" ]; then
@@ -111,45 +118,89 @@ run_workload()
     fi
     
     if [ "$func" = "chameleon" ]; then
-        workload_cmd="python /home/cc/functions/run_bench/normal_run/chameleon_.py 3500"
+        workload_cmd="python /home/cc/functions/run_bench/normal_run/chameleon_.py 6000"
     elif [ "$func" = "para_comp" ]; then
-        workload_cmd="python /home/cc/functions/run_bench/normal_run/para_comp.py 3000000000 12"
+        workload_cmd="python /home/cc/functions/run_bench/normal_run/para_comp.py 9000000000 12"
     elif [ "$func" = "img_proc" ]; then
-        workload_cmd="python /home/cc/functions/run_bench/normal_run/img_proc/img_proc.py 63mb"
+        workload_cmd="python /home/cc/functions/run_bench/normal_run/img_proc/img_proc.py 191mb"
     elif [ "$func" = "matmul_go" ]; then
-        workload_cmd="matmul 7000"
+        workload_cmd="matmul 13000"
     elif [ "$func" = "linpack_go" ]; then
-        workload_cmd="linpack 3000"
+        workload_cmd="linpack 7000"
     elif [ "$func" = "pagerank" ]; then
-        workload_cmd="python /home/cc/functions/run_bench/normal_run/graph_.py pagerank 4000000"
+        # local wl_args=$4
+        workload_cmd="python /home/cc/functions/run_bench/normal_run/graph_.py pagerank $wl_args $run_vtune $get_heatmap"
     elif [ "$func" = "bfs" ]; then
-        workload_cmd="python /home/cc/functions/run_bench/normal_run/graph_.py bfs 4000000"
+        # local wl_args=$4
+        workload_cmd="python /home/cc/functions/run_bench/normal_run/graph_.py bfs $wl_args $run_vtune $get_heatmap"
     elif [ "$func" = "mst" ]; then
-        workload_cmd="python /home/cc/functions/run_bench/normal_run/graph_.py mst 4000000"
+        # local wl_args=$4
+        workload_cmd="python /home/cc/functions/run_bench/normal_run/graph_.py mst $wl_args $run_vtune $get_heatmap"
+    # elif [ "$func" = "clusters" ]; then
+        # local wl_args=$4
+        # workload_cmd="python /home/cc/functions/run_bench/normal_run/graph_.py clusters $wl_args"
+    elif [ "$func" = "biconnect" ]; then
+        # local wl_args=$4
+        workload_cmd="python /home/cc/functions/run_bench/normal_run/graph_.py biconnect $wl_args $run_vtune $get_heatmap"
     elif [ "$func" = "gif" ]; then
-        workload_cmd="python /home/cc/functions/run_bench/normal_run/watermark/watermark.py 30mb to_gif"
+        workload_cmd="python /home/cc/functions/run_bench/normal_run/watermark/watermark.py concat to_gif"
     elif [ "$func" = "watermark" ]; then
-        workload_cmd="python /home/cc/functions/run_bench/normal_run/watermark/watermark.py 78_4k watermark"
+        workload_cmd="python /home/cc/functions/run_bench/normal_run/watermark/watermark.py 137_4k watermark"
     elif [ "$func" = "resnet18_train" ]; then
-        workload_cmd="python /home/cc/bm_test/cifar100/cifar100.py train resnet18 6000"
+        workload_cmd="python /home/cc/bm_test/cifar100/cifar100.py train resnet18 50000"
     elif [ "$func" = "squeezenet_train" ]; then
-        workload_cmd="python /home/cc/bm_test/cifar100/cifar100.py train squeezenet 10000"
+        workload_cmd="python /home/cc/bm_test/cifar100/cifar100.py train squeezenet 50000"
+    elif [ "$func" = "resnet152_train" ]; then
+        workload_cmd="python /home/cc/bm_test/cifar100/cifar100.py train resnet152 6000" 
+    elif [ "$func" = "vgg16_train" ]; then
+        workload_cmd="python /home/cc/bm_test/cifar100/cifar100.py train vgg16 50000"
+    elif [ "$func" = "attention56_train" ]; then
+        workload_cmd="python /home/cc/bm_test/cifar100/cifar100.py train attention56 40000"
     elif [ "$func" = "resnet152_serve" ]; then
-        workload_cmd="python /home/cc/bm_test/cifar100/cifar100.py serve resnet152 4000"
+        workload_cmd="python /home/cc/bm_test/cifar100/cifar100.py serve resnet152 10000"
+    elif [ "$func" = "inceptionv4_serve" ]; then
+        workload_cmd="python /home/cc/bm_test/cifar100/cifar100.py serve inceptionv4 10000"
+    elif [ "$func" = "resnet50_serve" ]; then
+        workload_cmd="python /home/cc/bm_test/cifar100/cifar100.py serve resnet50 10000"
+    elif [ "$func" = "squeezenet_serve" ]; then
+        workload_cmd="python /home/cc/bm_test/cifar100/cifar100.py serve squeezenet 10000"
+    elif [ "$func" = "vgg16_serve" ]; then
+        workload_cmd="python /home/cc/bm_test/cifar100/cifar100.py serve vgg16 10000"
+
+    # elif [ "$func" = "reddit_5k" ]; then
+    #     workload_cmd="python /home/cc/tudataset/tud_benchmark/unit_test.py REDDIT-MULTI-5K"
+    # elif [ "$func" = "FRANKENSTEIN" ]; then
+    #     workload_cmd="python /home/cc/tudataset/tud_benchmark/unit_test.py FRANKENSTEIN"
+    # elif [ "$func" = "COLLAB" ]; then
+    #     workload_cmd="python /home/cc/tudataset/tud_benchmark/unit_test.py COLLAB"
+    # elif [ "$func" = "Fingerprint" ]; then
+    #     workload_cmd="python /home/cc/tudataset/tud_benchmark/unit_test.py Fingerprint"
+    # elif [ "$func" = "NCI1" ]; then
+    #     workload_cmd="python /home/cc/tudataset/tud_benchmark/unit_test.py NCI1"
+    # elif [ "$func" = "DD" ]; then
+    #     workload_cmd="python /home/cc/tudataset/tud_benchmark/unit_test.py DD"
     else
         echo "wrong workload"; exit 1
     fi
-
-    $cmd_prefix $workload_cmd & 
+    # delete & if doing vtune, otherwise vtune doesn't know when to stop
+    $cmd_prefix $workload_cmd &
     check_pid=$!
     if [ "$check_pid" = "" ]; then
         echo "unable to get wl pid, something is wrong"; exit 1
     fi
-
+    # if [ "$run_vtune" = true ] ; then
+    #     if [ "$func" = "pagerank" ] || [ "$func" = "mst" ] || [ "$func" = "bfs" ] || [ "$func" = "biconnect" ]; then
+    #         vtune_pid=$(ps aux | grep 'damo' | grep -v grep | awk '{print $2}' | head -n 1)
+    #         while [ ! -z "" ]; then
+    #             echo "killing $perf_record_pid"
+    #             sudo kill -9 $perf_record_pid
+    #         fi
+    #     fi
+    # fi
 
     # run_trace_prefix="numactl --cpunodebind 1 -- "
     if [ "$get_rss" = true ] ; then
-        if [ "$func" = "pagerank" ] || [ "$func" = "mst" ] || [ "$func" = "bfs" ]; then
+        if [ "$func" = "pagerank" ] || [ "$func" = "mst" ] || [ "$func" = "bfs" ] || [ "$func" = "biconnect" ]; then
             perf_record_pid=$(ps aux | grep 'damo' | grep -v grep | awk '{print $2}' | head -n 1)
             if [ ! -z $perf_record_pid ]; then
                 echo "killing $perf_record_pid"
@@ -160,9 +211,9 @@ run_workload()
                 sleep 0.1
             done
             echo "found damo, start gen_rss"
-            gen_rss $check_pid $func &
+            gen_rss $check_pid $wl_folder &
         else
-            gen_rss $check_pid $func &
+            gen_rss $check_pid $wl_folder &
         fi
     fi
     if [ "$get_bw" = true ]; then
@@ -172,13 +223,13 @@ run_workload()
                 sleep 0.1
             done
             echo "found damo, start gen_bw"
-            gen_bw $check_pid $func &
+            gen_bw $check_pid $wl_folder &
         else
-            gen_bw $check_pid $func &
+            gen_bw $check_pid $wl_folder &
         fi
     fi
     if [ "$get_heatmap" = true ]; then
-        if [ "$func" = "pagerank" ] || [ "$func" = "mst" ] || [ "$func" = "bfs" ]; then
+        if [ "$func" = "pagerank" ] || [ "$func" = "mst" ] || [ "$func" = "bfs" ] || [ "$func" = "biconnect" ]; then
             # while [ -z "$(ps aux | grep 'damo' | grep -v grep | awk '{print $2}' | head -n 1)" ]
             # do
             #     echo "waiting for damo to start..."
@@ -191,9 +242,9 @@ run_workload()
                 sleep 0.1
             done
             sleep 3
-            gen_heatmap $check_pid $func false &
+            gen_heatmap $check_pid $wl_folder false &
         else
-            gen_heatmap $check_pid $func true &
+            gen_heatmap $check_pid $wl_folder true &
         fi
     fi
 }
@@ -201,4 +252,4 @@ run_workload()
 # $1: env <base, cxl>
 # $2: function to run <chameleon>
 echo 1 | sudo tee /proc/sys/vm/drop_caches
-run_workload $1 $2
+run_workload $1 $2 $3
